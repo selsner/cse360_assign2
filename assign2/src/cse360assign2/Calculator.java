d86d50670d25115753b40130464393cd8eded5b4
Shannon Elsner <Shannon Elsner@Cortnies>
2019-02-22 12:38:52 -0700

    Second version of Calculator, methods reflect calulator functions

--- assign2/src/cse360assign2/Calculator.java
+++ assign2/src/cse360assign2/Calculator.java
@@ -1,10 +1,9 @@
-/* Calculator, Initial version 
+/* Calculator, second version
  * Javadoc comment style used
- * Code unchanged
- * Shannon Elsner, 370 
- * CSE360, Monday section 
+ * Methods updated to perform like a calculator 
+ * Shannon Elsner 370
+ * CSE 360, Monday section
  */
-
 package cse360assign2;
 
 public class Calculator {
@@ -16,29 +15,39 @@ public class Calculator {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	//return current total
 	public int getTotal () 
 	{
-		return 0;
+		return total;
 	}
 	
+	//add the parameter to the total variable
 	public void add (int value) 
 	{
-		
+		total = total + value;
 	}
 	
+	//subtract the parameter from the total variable
 	public void subtract (int value) 
 	{
-		
+		total = total - value;
 	}
 	
+	//multiply the total by the parameter
 	public void multiply (int value) 
 	{
-		
+		total = total * value;
 	}
 	
+	/* divide the total by the parameter using integer division
+	 * If the parameter is zero, set the total to zero
+	 */ 
 	public void divide (int value) 
 	{
-		
+		if(value == 0)
+			total = 0;
+		else
+			total = total / value;
 	}
 	
 	public String getHistory () 
