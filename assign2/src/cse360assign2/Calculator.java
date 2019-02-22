e30104171b99e0e3a981f2dd4abcc36b22f00662
Shannon Elsner <Shannon Elsner@Cortnies>
2019-02-22 12:24:07 -0700

    Initial version of Calculator, no changes made to code

---
+++ assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,48 @@
+/* Calculator, Initial version 
+ * Javadoc comment style used
+ * Code unchanged
+ * Shannon Elsner, 370 
+ * CSE360, Monday section 
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	public void add (int value) 
+	{
+		
+	}
+	
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	public void divide (int value) 
+	{
+		
+	}
+	
+	public String getHistory () 
+	{
+		return "";
+	}
+}
